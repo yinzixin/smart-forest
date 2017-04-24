c37cee99ba905fc6ccfcb38ab43a84a61159008c
wangli <wangli1@adsage.com>
2017-04-24 08:48:26 +0800

    页面更新

--- html/js/editor.js
+++ html/js/editor.js
@@ -3,4 +3,23 @@
  */
 $(function(){
     $(".radios").uniform();
+        //$(".tab-container .tab-navs a").click(function(){
+        //    $(".tab-container .tab-navs a").removeClass("active");
+        //    //$(this).addClass("active");
+        //});
+
+    function resetTabNav(){
+        var st = $(window).scrollTop();
+        $(".tab-container .tab-navs a").each(function(i,nav){
+            var target = $($(nav).attr('href'));
+            if((target).position().top<st){
+                $(".tab-container .tab-navs a").removeClass("active");
+                $(nav).addClass('active');
+            }
+        })
+    }
+
+    $(window).scroll(resetTabNav);
+
+    resetTabNav();
 })
